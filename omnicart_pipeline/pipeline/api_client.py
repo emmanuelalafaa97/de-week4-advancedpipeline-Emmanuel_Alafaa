fb4e18ee1f5e773a44655078cbb633faa924b374
Manuel A <99599986+emmanuelalafaa97@users.noreply.github.com>
2025-11-02 13:52:10 +0100

    created and updated the api_client.py file

---
+++ omnicart_pipeline/pipeline/api_client.py
@@ -0,0 +1,77 @@
+import requests
+import logging
+
+# Configure basic logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+class APIClient:
+    """A simple wrapper for the fakestore API."""
+    
+    def __init__(self, base_url):
+         self.base_url = base_url
+
+    def get_user(self, user_id: int) -> dict:
+         """Fetches a single user by their ID."""
+         endpoint = f"/users/{user_id}"
+         url = self.base_url + endpoint
+
+         logging.info(f"Fetching user from {url}")
+         try:
+             response = requests.get(url)
+             response.raise_for_status() # Check for HTTP errors
+             return response.json()
+         except requests.exceptions.RequestException as e:
+             logging.error(f"API request failed: {e}")
+             # Return an empty dict or raise a custom exception
+             return {}
+
+    #function to fetch all users data from the API
+    def get_all_users(self) -> dict:
+         """Fetches all users by their ID."""
+         endpoint = f"/users/"
+         url = self.base_url + endpoint
+
+         logging.info(f"Fetching user from {url}")
+         try:
+             response = requests.get(url)
+             response.raise_for_status() # Check for HTTP errors
+             return response.json()
+         except requests.exceptions.RequestException as e:
+             logging.error(f"API request failed: {e}")
+             # Return an empty dict or raise a custom exception
+             return {}
+        
+
+    def get_product(self, product_id: int) -> dict:
+         """Fetches a single product by it's ID."""
+         endpoint = f"/products/{product_id}"      
+         url = self.base_url + endpoint
+
+         logging.info(f"Fetching user from {url}")
+         try:
+            response = requests.get(url)
+            response.raise_for_status()   # Check for HTTP errors
+            return response.json()
+
+         except requests.exceptions.RequestException as e:
+             logging.error(f"API request failed: {e}")
+             # Return an empty dict or raise a custom exception
+             return {}
+
+    def get_all_products(self) -> dict:
+         """Fetches all products by their ID."""
+         endpoint = f"/products/"
+         url = self.base_url + endpoint
+
+         logging.info(f"Fetching products from {url}")
+         try:
+             response = requests.get(url)
+             response.raise_for_status() # Check for HTTP errors
+             return response.json()
+         except requests.exceptions.RequestException as e:
+             logging.error(f"API request failed: {e}")
+             # Return an empty dict or raise a custom exception
+             return {}
+
+
+         
